589838e7b8b0f2e9b3ee67df1fc58c0eacb0492f
bilali <mercan_shark@windowslive.com>
2023-10-25 09:46:39 +0300

    service update

--- services/auth/app/Service/UserService.js
+++ services/auth/app/Service/UserService.js
@@ -1,15 +1,12 @@
 import AuthRepository from "../Repositry/AuthRepository.js";
-import bcrypt from "bcryptjs";
-import AuthService from "./AuthService.js";
+import {generateHashPassword} from "../Utils/Bcrypt.js";
 
 
 export default class UserService {
     async createUser(userData) {
         const authRepository = new AuthRepository()
 
-        const authService = new AuthService()
-
-        userData.password = await authService.generateHashPassword(userData.password)
+        userData.password = await generateHashPassword(userData.password)
 
         return await authRepository.createUser(userData)
     }
