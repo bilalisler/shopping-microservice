e45680981522cfed7041ebf86b0337a76c9e548b
bilali <mercan_shark@windowslive.com>
2023-10-29 07:45:54 +0300

    Adds JWT Guard, JWT decode mechanism, unauthenticated definition for Handler.php Updates api endpoint Removes unnecessary files

---
+++ services/product/app/Service/JWT.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Service;
+
+use UnexpectedValueException;
+
+class JWT
+{
+    /**
+     * @param string $jwt The JWT
+     * @param string|null $key The secret key
+     * @param bool $verify Don't skip verification process
+     *
+     * @return object The JWT's payload as a PHP object
+     * @return object
+     * @throws \Exception
+     */
+    public static function decode(string $jwt, string $key = null, bool $verify = true): object
+    {
+        $tks = explode('.', $jwt);
+        if (count($tks) != 3) {
+            throw new UnexpectedValueException('Wrong number of segments');
+        }
+        list($headb64, $payloadb64, $cryptob64) = $tks;
+        if (null === ($header = JWT::jsonDecode(JWT::urlsafeB64Decode($headb64)))
+        ) {
+            throw new UnexpectedValueException('Invalid segment encoding');
+        }
+        if (null === $payload = JWT::jsonDecode(JWT::urlsafeB64Decode($payloadb64))
+        ) {
+            throw new UnexpectedValueException('Invalid segment encoding');
+        }
+        $sig = JWT::urlsafeB64Decode($cryptob64);
+        if ($verify) {
+            if (empty($header->alg)) {
+                throw new \Exception('Empty algorithm');
+            }
+            if ($sig != JWT::sign("$headb64.$payloadb64", $key, $header->alg)) {
+                throw new UnexpectedValueException('Signature verification failed');
+            }
+        }
+        return $payload;
+    }
+
+
+    /**
+     * @param string $msg The message to sign
+     * @param string $key The secret key
+     * @param string $method The signing algorithm
+     *
+     * @return string An encrypted message
+     */
+    public static function sign(string $msg, string $key, string $method = 'HS256'): string
+    {
+        $methods = array(
+            'HS256' => 'sha256',
+            'HS384' => 'sha384',
+            'HS512' => 'sha512',
+        );
+        if (empty($methods[$method])) {
+            throw new \Exception('Algorithm not supported');
+        }
+        return hash_hmac($methods[$method], $msg, $key, true);
+    }
+
+    /**
+     * @param string $input JSON string
+     *
+     * @return object Object representation of JSON string
+     */
+    public static function jsonDecode(string $input): object
+    {
+        $obj = json_decode($input);
+        if (function_exists('json_last_error') && $errno = json_last_error()) {
+            JWT::handleJsonError($errno);
+        } else if ($obj === null && $input !== 'null') {
+            throw new \Exception('Null result with non-null input');
+        }
+        return $obj;
+    }
+
+    /**
+     * @param string $input A base64 encoded string
+     *
+     * @return string A decoded string
+     */
+    public static function urlsafeB64Decode(string $input): string
+    {
+        $remainder = strlen($input) % 4;
+        if ($remainder) {
+            $padlen = 4 - $remainder;
+            $input .= str_repeat('=', $padlen);
+        }
+        return base64_decode(strtr($input, '-_', '+/'));
+    }
+
+    /**
+     * @param int $errno An error number from json_last_error()
+     *
+     * @return void
+     */
+    private static function handleJsonError(int $errno): void
+    {
+        $messages = array(
+            JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+            JSON_ERROR_CTRL_CHAR => 'Unexpected control character found',
+            JSON_ERROR_SYNTAX => 'Syntax error, malformed JSON'
+        );
+        throw new \Exception(isset($messages[$errno])
+            ? $messages[$errno]
+            : 'Unknown JSON error: ' . $errno
+        );
+    }
+
+}
--- services/product/routes/api.php
+++ services/product/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureTokenIsValid;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::middleware([])
+    ->prefix('products')
+    ->group(function () {
+        Route::get('/', function (){
+            dd('test');
+        });
+    });
