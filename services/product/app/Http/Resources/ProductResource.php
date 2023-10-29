62fa4d6b5ea96da4084f5bb9d66b64de903076c2
bilali <mercan_shark@windowslive.com>
2023-10-29 20:17:53 +0300

    galleries and categories were added

--- services/product/app/Http/Resources/ProductResource.php
+++ services/product/app/Http/Resources/ProductResource.php
@@ -23,7 +23,9 @@ class ProductResource extends JsonResource
             "description" => $this->description,
             "status" => $this->status,
             "created_at" => $this->created_at,
-            "updated_at" => $this->updated_at
+            "updated_at" => $this->updated_at,
+            'categories' => CategoryResource::collection($this->categories),
+            'galleries' => GalleryResource::collection($this->galleries)
         ];
     }
 }
