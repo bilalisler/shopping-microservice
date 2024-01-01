f3052415e1b8d6a657d78d4d6579bb905c6e6816
bilali <mercan_shark@windowslive.com>
2024-01-01 22:24:04 +0300

    add health/check endpoint for cart service

--- services/cart/src/Routes/Api.ts
+++ services/cart/src/Routes/Api.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import {Request, Response, Router} from 'express';
 import validateBody from "../Middleware/RequestValidator";
 import CommentController from '../Controllers/CartController';
 import AddToCartRequest from "../Requests/AddToCartRequest";
@@ -8,6 +8,11 @@ import DeleteFromCartRequest from "../Requests/DeleteFromCartRequest";
 const router = Router();
 
 
+router.get('/health/check', async function (req: Request, res: Response) {
+    res.json({
+        "status": "Success Cart"
+    })
+})
 router.get('/', CommentController.show);
 router.post('/', validateBody(AddToCartRequest), CommentController.add);
 router.put('/', validateBody(UpdateToCartRequest), CommentController.update);
