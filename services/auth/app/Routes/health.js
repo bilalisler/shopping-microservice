42884134ea59c5f49b55c2c99d1bc9d603d02068
bilali <mercan_shark@windowslive.com>
2024-01-01 20:07:57 +0300

    minor update

--- services/auth/app/Routes/health.js
+++ services/auth/app/Routes/health.js
@@ -1,6 +1,6 @@
 export default async function routes(fastify, options) {
     fastify.get('/health/check', async (request, reply) => {
-        return reply.send({OK: 'Success'})
+        return reply.send({OK: 'Success Auth'})
     })
 }
 
