93087ea01d45565221c5bb1be5fa658eb25fa897
bilali <mercan_shark@windowslive.com>
2024-01-01 22:24:18 +0300

    add health/check endpoint for comment service

--- services/comment/src/Routes/Api.ts
+++ services/comment/src/Routes/Api.ts
@@ -1,14 +1,17 @@
-import {Router} from 'express';
+import {Request, Response, Router} from 'express';
 import validateBody from "../Middleware/RequestValidator";
 import CommentController from '../Controllers/CommentController';
 import CreateCommentRequest from "../Requests/CreateCommentRequest";
 import UpdateCommentRequest from "../Requests/UpdateCommentRequest";
 import DeleteCommentRequest from "../Requests/DeleteCommentRequest";
-// import * as expressJwt from 'express-jwt';
 
 const router = Router();
 
-
+router.get('/health/check', async function (req: Request, res: Response) {
+    res.json({
+        "status": "Success Comment"
+    })
+});
 router.get('/', CommentController.show);
 router.post('/', validateBody(CreateCommentRequest), CommentController.create);
 router.put('/', validateBody(UpdateCommentRequest), CommentController.update);
